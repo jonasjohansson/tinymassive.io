d50ab9aeff19a8085f8cfddda19065b50d34082d
Owen Hindley <owenhindley@hotmail.com>
2018-11-04 22:37:30 +0000

    add Unity syphon project / tester

---
+++ unity/TinyMassiveSyphonClient/Assets/Scripts/CropSyphonToTexture.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Events;
+using TMPro;
+
+public class CropSyphonToTexture : MonoBehaviour {
+
+	public TMP_InputField yOffsetInput;
+	public TMP_InputField captureWidthInput;
+	public TMP_InputField captureHeightInput;
+	public TMP_InputField appNameInput;
+
+	public RenderTexture sourceTexture;
+	public RenderTexture targetTexture;
+
+	public int Width = 77;
+	public int Height = 13;
+
+	public int yOffset = 0;
+
+	private Material material;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void UpdateSettings(){
+
+		Width = int.Parse(captureWidthInput.text);
+		Height = int.Parse(captureHeightInput.text);
+
+		yOffset = int.Parse(yOffsetInput.text);
+
+		
+	}
+}
---
+++ unity/TinyMassiveSyphonClient/Assets/Scripts/ForceWindowed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceWindowed : MonoBehaviour {
+
+	public int width = 1024;
+	public int height = 1024;
+
+
+	// Use this for initialization
+	void Start () {
+		Screen.fullScreen = false;
+		Screen.SetResolution(width, height, false, 30);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
